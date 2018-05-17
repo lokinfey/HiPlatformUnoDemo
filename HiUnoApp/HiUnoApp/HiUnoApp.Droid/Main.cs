51c572ee401f646068742f70cbf766a15ae553ac
Kinfey <lokinfey@outlook.com>
2018-05-17 11:24:39 +0800

    Init
    Init

---
+++ HiUnoApp/HiUnoApp/HiUnoApp.Droid/Main.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Nostra13.Universalimageloader.Core;
+using Windows.UI.Xaml.Media;
+
+namespace HiUnoApp.Droid
+{
+    [global::Android.App.ApplicationAttribute(
+        Label = "@string/ApplicationName",
+        LargeHeap = true,
+        HardwareAccelerated = true,
+        Theme = "@style/AppTheme"
+    )]
+    public class Application : Windows.UI.Xaml.NativeApplication
+    {
+        public Application(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(new App(), javaReference, transfer)
+        {
+            ConfigureUniversalImageLoader();
+        }
+
+        private void ConfigureUniversalImageLoader()
+        {
+            ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(Context).Build();
+
+            ImageLoader.Instance.Init(config);
+
+            ImageSource.DefaultImageLoader = ImageLoader.Instance.LoadImageAsync;
+        }
+    }
+}
---
+++ HiUnoApp/HiUnoApp/HiUnoApp.Shared/Model/Video.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uno;
+
+namespace HiUnoApp.Shared.Model
+{
+#if __ANDROID__ || __IOS__ || __WASM__
+    [Preserve]
+#endif
+    public class Video
+    {
+        public int VideoID { get; set; }
+        public string VideoName { get; set; }
+        public string Teacher { get; set; }
+        public string VideoImage { get; set; }
+    }
+
+    public class VideoContent
+    {
+        public static IList<Video> GetVideoList()
+        {
+            var videos = new List<Video>
+            {
+                new Video { VideoID = 1, VideoName = "Demo1", Teacher = "A", VideoImage = "http://jpkc.gdou.com/wechatlive/content/img/cover/th1006402.png" },
+                new Video { VideoID = 2, VideoName = "Demo2", Teacher = "B", VideoImage = "http://jpkc.gdou.com/wechatlive/content/img/cover/th1006402.png" },
+                new Video { VideoID = 3, VideoName = "Demo3", Teacher = "C", VideoImage = "http://jpkc.gdou.com/wechatlive/content/img/cover/th1006402.png" }
+            };
+
+            return videos;
+        }
+    }
+}
